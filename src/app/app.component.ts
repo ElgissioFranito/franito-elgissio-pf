48228fcbf49c54387d850d60f7824a7e116421b0
ElgissioFranito <mistera.inwy@gmail.com>
2025-01-08 04:43:36 +0300

    hero-section : first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HeroComponent } from './pages/hero/hero.component';
+import { SharedService } from './services/shared.service';
+import { NgClass } from '@angular/common';
+import { AboutComponent } from './pages/about/about.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     // RouterOutlet,
     NavBarComponent,
-    HeroComponent
+    HeroComponent,
+    NgClass,
+    AboutComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'franito-elgissio-pf';
+
+  sharedService = inject(SharedService);
 
   ngOnInit(): void {
     this.setDefaultTheme();
   }
 
   setDefaultTheme() {
+    console.log("setDefaultTheme");
+
     const root = document.documentElement;
 
     // Vérifier si un thème est déjà défini dans le stockage local
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme) {
       root.setAttribute('data-theme', savedTheme);
+
+      this.sharedService.theme.set(savedTheme);
+
       return;
     }
 
     // Récupérer le thème préféré du système (clair ou sombre)
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     root.setAttribute('data-theme', prefersDark ? 'dark' : 'light');
-    console.log(root.getAttribute('data-theme'));
-    
+
+    const defaultTheme = root.getAttribute('data-theme');
+
+    this.sharedService.theme.set(defaultTheme ?? 'dark');
+
+    console.log("this.sharedService.theme ", this.sharedService.theme());
+
   }
 
   toggleTheme() {
@@ -43,6 +60,23 @@ export class AppComponent implements OnInit{
     const currentTheme = root.getAttribute('data-theme');
     root.setAttribute('data-theme', currentTheme === 'dark' ? 'light' : 'dark');
     localStorage.setItem('theme', currentTheme === 'dark' ? 'light' : 'dark');
+
+    if (root.getAttribute('data-theme') === 'light') {
+      this.sharedService.theme.set('light');
+    } else {
+      this.sharedService.theme.set('dark');
+    }
+    console.log("this.sharedService.theme ", this.sharedService.theme());
   }
-  
+
+  onMouseMove(event: MouseEvent) {
+    const { currentTarget: target } = event as any;
+    const rect = target?.getBoundingClientRect(),
+      x = event.clientX - rect.left,
+      y = event.clientY - rect.top;
+
+    target?.style.setProperty('--mouse-x', `${x}px`);
+    target?.style.setProperty('--mouse-y', `${y}px`);
+  }
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,46 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        textColor : "var(--text-color)",
-        primary: "var(--primary)",
-        secondary: "var(--secondary)",
-        tertiary: "var(--tertiary)",
-        four: "var(--four)",
-        five: "var(--five)",
+        textColor : {
+          "DEFAULT" : "var(--text-color)",
+          "10" : "var(--text-color-10)",
+          "20" : "var(--text-color-20)",
+          "50" : "var(--text-color-50)",
+        },
+        primary: {
+          "DEFAULT" : "var(--primary)",
+          "10" : "var(--primary-10)",
+          "20" : "var(--primary-20)",
+          "50" : "var(--primary-50)",
+        },
+        secondary: {
+          "DEFAULT" : "var(--secondary)",
+          "10" : "var(--secondary-10)",
+          "20" : "var(--secondary-20)",
+          "50" : "var(--secondary-50)",
+        },
+        tertiary: {
+          "DEFAULT" : "var(--tertiary)",
+          "10" : "var(--tertiary-10)",
+          "20" : "var(--tertiary-20)",
+          "50" : "var(--tertiary-50)",
+        },
+        four: {
+          "DEFAULT" : "var(--four)",
+          "10" : "var(--four-10)",
+          "20" : "var(--four-20)",
+          "50" : "var(--four-50)",
+        },
+        five: {
+          "DEFAULT" : "var(--five)",
+          "10" : "var(--five-10)",
+          "20" : "var(--five-20)",
+          "50" : "var(--five-50)",
+        },
       },
 
       transitionProperty: {
-        colors: 'background-color, border-color, color, fill, stroke',
+        colors: 'background-color, border-color, color, fill, stroke, border, all',
       },
     },
   },
