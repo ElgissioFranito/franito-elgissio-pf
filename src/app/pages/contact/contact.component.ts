7db068113cc8366eedb26e03b2d993d1b12a6546
ElgissioFranito <mistera.inwy@gmail.com>
2025-01-14 12:50:30 +0300

    contact animations

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,21 +1,24 @@
+import { NgClass } from '@angular/common';
 import { Component, inject, NgZone } from '@angular/core';
 import gsap from 'gsap';
+import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
   mail = "franitorandriamanarinna@gmail.com";
   
+  sharedService = inject(SharedService);
   zone = inject(NgZone);
 
   ngAfterViewInit() {
     this.zone.runOutsideAngular(() => {
       setTimeout(() => {
-        document.querySelectorAll('.title').forEach((element) => {
+        document.querySelectorAll('.title, .subtitle').forEach((element) => {
           gsap.from(element, {
             scrollTrigger: {
               trigger: element,
@@ -32,7 +35,7 @@ export class ContactComponent {
           });
         })
 
-        document.querySelectorAll('.form-section form div, .form-section form h2').forEach((element) => {
+        document.querySelectorAll('.form-section form div, .form-section .form-title').forEach((element) => {
           gsap.from(element, {
             scrollTrigger: {
               trigger: element,
@@ -71,17 +74,19 @@ export class ContactComponent {
   
   onMouseMove(event: MouseEvent) {
     const { currentTarget: target } = event as any;
-    // const rect = target?.getBoundingClientRect(),
-    //   x = event.clientX - rect.left,
-    //   y = event.clientY - rect.top;
+    const rect = target?.getBoundingClientRect(),
+      x = event.clientX - rect.left,
+      y = event.clientY - rect.top;
 
-    // target?.style.setProperty('--mouse-x', `${x}px`);
-    // target?.style.setProperty('--mouse-y', `${y}px`);
+    target?.style.setProperty('--mouse-x', `${x}px`);
+    target?.style.setProperty('--mouse-y', `${y}px`);
 
-    let mouseX = event.pageX - target.offsetLeft;
-    let mouseY = event.pageY - target.offsetTop;
+    // let mouseX = event.pageX - target.offsetLeft;
+    // let mouseY = event.pageY - target.offsetTop;
 
-    target.style.setProperty('--mouse-x', `${mouseX}px`);
-    target.style.setProperty('--mouse-y', `${mouseY}px`);
+    // target.style.setProperty('--mouse-x', `${mouseX}px`);
+    // target.style.setProperty('--mouse-y', `${mouseY}px`);
+    // console.log(mouseX, mouseY);
+    
   }
 }
